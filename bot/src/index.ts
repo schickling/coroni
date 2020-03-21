3d95444b030896bc9dcb7ae1a7d1c09f35040261
schickling <schickling.j@gmail.com>
2020-03-21 11:45:27 +0100

    added initial bot boilerplate

---
+++ bot/src/index.ts
@@ -0,0 +1,8 @@
+import Telegraf from 'telegraf'
+
+const bot = new Telegraf(process.env.BOT_TOKEN!)
+bot.start(ctx => ctx.reply('Welcome'))
+bot.help(ctx => ctx.reply('Send me a sticker'))
+bot.on('sticker', ctx => ctx.reply('👍'))
+bot.hears('hi', ctx => ctx.reply('Hey there'))
+bot.launch()
