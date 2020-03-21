6040b74d81d4c68cf26a60f4e2fc4e4f94aa31c3
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2020-03-21 14:15:36 +0100

    Added scraping and cases data.

---
+++ backend/scrape/scrape.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+document = requests.get("https://www.coronazaehler.de/").text
+# yay fix broken encoding.
+document = document.encode('latin-1').decode('utf-8')
+soup = BeautifulSoup(document, "lxml")
+
+cards = soup.findAll(("div", {"class": "card"}))
+
+data = {}
+
+for card in cards:
+    header = card.find('h4')
+    table = card.find('table')
+
+    if header is None or table is None:
+        continue
+
+    state = header.getText()
+
+    if state == "coronazaehler.de":
+        state = 'Deutschland'
+
+    rows = table.findAll("tr")
+
+    regionData = {}
+    
+    for row in rows:
+        cols = row.findAll("td")
+        # ignore table header (th)
+        if len(cols) > 2:
+            region = cols[0].getText()
+            casesPerThousand = float(cols[1].getText()) / 100
+            regionData[region] = casesPerThousand
+
+    data[state] = regionData
+
+print(json.dumps(data, indent=1))
